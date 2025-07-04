7dca5acaffb23c59b5325ed803ff891c948ecd4f
chilakamrajesh <rajeshreddychilakam@gmail.com>
2025-07-04 20:44:00 +0530

    not is

---
+++ fri-4-operators/identifier operator/one.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+c=10
+print(a is b)
+print(a is not b)
+print(a is c)
+
+
+## here we just use is and not iss
---
+++ fri-4-operators/identifier operator/two.py
@@ -0,0 +1,6 @@
+eid1=415
+eid2=420
+eid3=415
+print(eid1 is not eid2)
+print(eid1 is eid2)
+print(eid1 is eid3)
